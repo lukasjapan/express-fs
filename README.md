3623594f9e31525e457392c11e885c8d371ffd3e
Lukas <lukas@cvguy.de>
2018-10-20 19:45:09 +0900

    autoformat

--- README.md
+++ README.md
@@ -6,39 +6,40 @@ Let's RESTify your filesystem
 
 ## Usage
 
-```./bin/expose [path] [port]``` will serve  path (~ by default) at port (8000 by default).
+`./bin/expose [path] [port]` will serve path (~ by default) at port (8000 by default).
 
-As *exposefs* has been built as an express middleware, you can use it to your existing express application.
+As _exposefs_ has been built as an express middleware, you can use it to your existing express application.
 
 ```javascript
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var exposefs = require('exposefs');
+var exposefs = require("exposefs");
 
-app.use('/fs', exposefs({basepath: "/home/"}));
+app.use("/fs", exposefs({ basepath: "/home/" }));
 
 /* your routing definition */
 
-app.listen(8000)
+app.listen(8000);
 ```
 
 The code above will serve your `home` directory under `/fs` url.
 
 ## Exposed API
 
-| action | verb | operation | note |
-|--------|------|-----------|---------|
-| List Directory | `GET` | readdir |
-| Retrieve file | `GET` | read |
-| Stat file | `GET` | stat | must use `?stat` |
-| Follow a file | `GET` | follow | must use `?follow` |
-| Create File | `POST` | write |
-| Create Directory | `POST` | mkdir | file name must finish with `/` |
-| Append to File | `PUT` | write |
-| Update file timestamps | `PATCH` | utimes |
-| Remove a file or directory | `DELETE` | unlink |
+| action                     | verb     | operation | note                           |
+| -------------------------- | -------- | --------- | ------------------------------ |
+| List Directory             | `GET`    | readdir   |
+| Retrieve file              | `GET`    | read      |
+| Stat file                  | `GET`    | stat      | must use `?stat`               |
+| Follow a file              | `GET`    | follow    | must use `?follow`             |
+| Create File                | `POST`   | write     |
+| Create Directory           | `POST`   | mkdir     | file name must finish with `/` |
+| Append to File             | `PUT`    | write     |
+| Update file timestamps     | `PATCH`  | utimes    |
+| Remove a file or directory | `DELETE` | unlink    |
 
 ## TODO:
-* Testing
-* Add different backends (dropbox, s3, etc)
+
+- Testing
+- Add different backends (dropbox, s3, etc)
--- test/filesystem.test.js
+++ test/filesystem.test.js
@@ -1,18 +1,17 @@
-var mocha = require('mocha'),
-    assert = require('assert');
+var mocha = require("mocha"),
+  assert = require("assert");
 
-var request = require('supertest'),
-    __fs = require('fs');
-    Fs = require('fake-fs');
+var request = require("supertest"),
+  __fs = require("fs");
+Fs = require("fake-fs");
 
-var express = require('express'),
-    app = express(),
-    exposefs = require('../index');
-app.use('/', exposefs( {backend: 'filesystem', basepath: '/'} ));
-
+var express = require("express"),
+  app = express(),
+  exposefs = require("../index");
+app.use("/", exposefs({ backend: "filesystem", basepath: "/" }));
 
 var fs = null;
-describe('Filesystem', function() {
+describe("Filesystem", function() {
   beforeEach(function() {
     fs = new Fs();
     fs.patch();
@@ -22,33 +21,38 @@ describe('Filesystem', function() {
     fs.unpatch();
   });
 
-  describe('GET', function() {
-    context('/directory', function() {
+  describe("GET", function() {
+    context("/directory", function() {
       it("returns the content of directory", function(done) {
-        var content = ['foo', 'bar'];
+        var content = ["foo", "bar"];
         content.forEach(function(f) {
           fs.dir("/" + f);
         });
 
-        request(app).get('/').expect(200, JSON.stringify(content), done);
+        request(app)
+          .get("/")
+          .expect(200, JSON.stringify(content), done);
       });
     });
 
     context("/file", function() {
       beforeEach(function() {
-        this.content = 'content of file';
-        fs.file('/file', this.content);
+        this.content = "content of file";
+        fs.file("/file", this.content);
       });
 
       it("returns the content of file", function(done) {
-        request(app).get('/file').expect(200, this.content, done);
+        request(app)
+          .get("/file")
+          .expect(200, this.content, done);
       });
 
       context("?stat", function() {
         it("returns the stat of a file", function(done) {
-          request(app).get('/file?stat')
+          request(app)
+            .get("/file?stat")
             .expect(200, function() {
-              JSON.stringify(fs.statSync('/file'));
+              JSON.stringify(fs.statSync("/file"));
               done();
             });
         });
@@ -57,12 +61,13 @@ describe('Filesystem', function() {
   });
 
   describe("POST", function() {
-    context('/directory/', function() {
+    context("/directory/", function() {
       it("creates a directory", function(done) {
-        request(app).post('/directory/')
+        request(app)
+          .post("/directory/")
           .expect(201)
           .end(function() {
-            assert(fs.statSync('/directory').isDirectory());
+            assert(fs.statSync("/directory").isDirectory());
             done();
           });
       });
@@ -74,10 +79,12 @@ describe('Filesystem', function() {
       });
 
       it("creates a file", function(done) {
-        var file = '/tmp/.exposefs_tmp_file';
-        var data = 'deleteme';
+        var file = "/tmp/.exposefs_tmp_file";
+        var data = "deleteme";
 
-        request(app).post(file).send(data)
+        request(app)
+          .post(file)
+          .send(data)
           .expect(201, function() {
             assert.equal(__fs.readFileSync(file), data);
             __fs.unlink(file);
@@ -87,35 +94,35 @@ describe('Filesystem', function() {
     });
   });
 
-	describe("PUT", function() {
-		beforeEach(function() {
-			fs.unpatch();
-		});
+  describe("PUT", function() {
+    beforeEach(function() {
+      fs.unpatch();
+    });
 
-		it("appends data to a file", function(done) {
-			var file = '/tmp/.exposefs_tmp_file';
-			__fs.writeFileSync(file, 'foo');
+    it("appends data to a file", function(done) {
+      var file = "/tmp/.exposefs_tmp_file";
+      __fs.writeFileSync(file, "foo");
 
-			request(app).put(file).send('bar')
-				.expect(200, function() {
-					assert.equal(
-						'foobar', __fs.readFileSync(file).toString()
-					);
-					__fs.unlink(file);
-					done();
-				});
-		});
-	});
+      request(app)
+        .put(file)
+        .send("bar")
+        .expect(200, function() {
+          assert.equal("foobar", __fs.readFileSync(file).toString());
+          __fs.unlink(file);
+          done();
+        });
+    });
+  });
 
-	describe("DELETE", function() {
-		it("removes file", function(done) {
-			fs.file('/file');
-			request(app).delete('/file')
-				.expect(200, function() {
-					assert.equal(fs.existsSync('/file'), false);
-					done();
-				});
-		});
-	});
-
+  describe("DELETE", function() {
+    it("removes file", function(done) {
+      fs.file("/file");
+      request(app)
+        .delete("/file")
+        .expect(200, function() {
+          assert.equal(fs.existsSync("/file"), false);
+          done();
+        });
+    });
+  });
 });
